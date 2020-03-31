32ce11e6e64427f96d9679b9c7590509d57e2af9
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-03-31 20:03:33 +0530

    create main class and add rollnumber and name to display

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -7,9 +7,9 @@ import problem3.myqueue.MyPriorityQueue;
 public class MyMain {
     public static void main(String[] args) {
         MyPriorityQueue mpq = new MyPriorityQueue();
-        mpq.push("ashi", 1);
-        mpq.push("anshika", 2);
-        mpq.push("anamika", 3);
+        mpq.push("shiv", 1);
+        mpq.push("shivam", 2);
+        mpq.push("shivansh", 3);
         mpq.display();
     }
 }
