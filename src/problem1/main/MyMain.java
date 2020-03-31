aabf5247e1384c4e1f7ae86c31bc1b68ac65616c
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-03-31 19:27:03 +0530

    create main class of problem 1

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,21 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree mbst = new MyBinarySearchTree();
+        mbst.insert(85);
+        mbst.insert(35);
+        mbst.insert(40);
+        mbst.insert(16);
+        mbst.insert(21);
+        mbst.insert(10);
+        mbst.insert(21);
+        mbst.insert(2);
+        mbst.display(mbst.getRoot(), mbst.getM());
+        mbst.newDisplay();
+
+    }
 }
